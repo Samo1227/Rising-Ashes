5173958da995a93af43986e17f2a364ecc97cada
Monster_Demon <reeceoh1234@gmail.com>
2019-01-31 15:46:56 +0000

    test

--- Data_Void/Assets/Script/Bot Making Scripts/Bot_Modifier.cs
+++ Data_Void/Assets/Script/Bot Making Scripts/Bot_Modifier.cs
@@ -221,15 +221,25 @@ public class Bot_Modifier : MonoBehaviour
 
         go_temp = Instantiate(go_ingame_bot,new Vector3(0,1,0),Quaternion.identity) as GameObject;
 
+
+
         //changes the name of the robot
-        go_temp.name = "Robot_" + int_bots_spawned;
+        //go_temp.name = "Robot_" + int_bots_spawned;
         //gets script from robot
         pb_in_game = go_temp.GetComponent<Playable_Bot>();
         //spawns the same type of parts on new robot that are on this script
         for(int i = 0; i < int_part_array_max; i++)
+        pr_in_game = go_temp.GetComponent<PlayerRobot>();
+
+        CSGameManager.gameManager.AddRobot(0, 0, pr_in_game);
+        //spawns the same type of parts on new robot that are on this script
+        for (int i = 0; i < int_part_array_max; i++)
+
         {
             pb_in_game.int_arr_parts[i] = int_body_type[i];
         }
+
+        CSGameManager.gameManager.AddRobot(0,0, pr_in_game);
 
     }
 
--- Data_Void/Assets/Script/CSGameManager.cs
+++ Data_Void/Assets/Script/CSGameManager.cs
@@ -225,10 +225,14 @@ public class CSGameManager : MonoBehaviour {
     #endregion
     //---------------------------------------------------
     #region Add PR
+<<<<<<< HEAD
     public void AddRobot(int cX, int cZ)
+=======
+    public void AddRobot(int cX, int cZ, PlayerRobot pr_made)
+>>>>>>> parent of dad8b6b... Bot Builder and core game combined
     {
         PlayerRobot tRo = null;
-        tRo = Instantiate(pr_PC);
+        tRo = Instantiate(pr_made);
         tRo.transform.position = new Vector3(cX, transform.position.y + 1f, cZ);
         tRo.int_x = cX;
         tRo.int_z = cZ;
