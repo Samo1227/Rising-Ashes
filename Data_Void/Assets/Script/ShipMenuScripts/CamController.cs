20a70e5d5cca21c604714848c4fe0573a16756d9
DarrenCrisp <35225549+DarrenCrisp@users.noreply.github.com>
2019-02-19 15:46:10 +0000

    added a test scene for the Ship

---
+++ Data_Void/Assets/Script/ShipMenuScripts/CamController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum Position
+{
+    north,
+    south,
+    east,
+    west,
+}
+
+public class CamController : MonoBehaviour
+{
+    public Position position;
+    public GameObject go_CamCont;
+    public float fl_Min_MaxPos;
+    public float fl_CamMoveSpeed=5f;
+
+    private void OnMouseOver()
+    {
+        MoveCam();
+    }
+
+    public void MoveCam()
+    {
+        switch (position)
+        {
+            case (Position.north):
+                if(go_CamCont.transform.position.y <= fl_Min_MaxPos)
+                {
+                    go_CamCont.transform.position += new Vector3(0, 1, 0)*fl_CamMoveSpeed * Time.deltaTime;
+                }
+                return;
+            case (Position.south):
+                if (go_CamCont.transform.position.y >= -fl_Min_MaxPos)
+                {
+                    go_CamCont.transform.position += new Vector3(0, -1, 0)*fl_CamMoveSpeed * Time.deltaTime;
+                }
+                return;
+            case (Position.east):
+                if (go_CamCont.transform.position.y <= fl_Min_MaxPos)
+                {
+                    go_CamCont.transform.position += new Vector3(1, 0, 0) * fl_CamMoveSpeed * Time.deltaTime;
+                }
+                return;
+            case (Position.west):
+                if (go_CamCont.transform.position.y >= -fl_Min_MaxPos)
+                {
+                    go_CamCont.transform.position += new Vector3(-1, 0, 0) * fl_CamMoveSpeed * Time.deltaTime;
+                }
+                return;
+        }
+    }
+}
