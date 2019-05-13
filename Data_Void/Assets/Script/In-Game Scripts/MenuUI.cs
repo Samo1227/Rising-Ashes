edfcdef414fb39c0fe03607b56469c818212a3cc
Darren Lee Crisp <ispdarrencrisp@hotmail.com>
2019-05-13 10:42:54 +0100

    Menu UI Shell

---
+++ Data_Void/Assets/Script/In-Game Scripts/MenuUI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuUI : MonoBehaviour
+{
+    public GameObject im_MenuHolder;
+    public GameObject im_HelpScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        im_MenuHolder = gameObject.transform.GetChild(1).gameObject;
+        im_HelpScreen = im_MenuHolder.gameObject.transform.GetChild(4).gameObject;
+    }
+
+    public void OpenMenuHolder()
+    {
+        im_MenuHolder.SetActive(true);
+    }
+    public void CloseMenuHolder()
+    {
+        im_MenuHolder.SetActive(false);
+    }
+    public void OpenHelpScreen()
+    {
+        im_HelpScreen.SetActive(true);
+    }
+    public void CloseHelpScreen()
+    {
+        im_HelpScreen.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Retreat()
+    {
+        SceneManager.LoadScene("ShipMenu");
+    }
+}
