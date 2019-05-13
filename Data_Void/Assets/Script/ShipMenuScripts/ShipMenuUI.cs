900a45f803588011e360472837b1f8cce55f69e1
Darren Lee Crisp <ispdarrencrisp@hotmail.com>
2019-05-13 14:23:20 +0100

    level select changes

---
+++ Data_Void/Assets/Script/ShipMenuScripts/ShipMenuUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShipMenuUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+  
+}
